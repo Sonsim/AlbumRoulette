b33c8e338cd54e3fc617969eea2582392a082a4d
Sonsim <141270918+Sonsim@users.noreply.github.com>
2024-02-27 11:11:34 +0100

    Added error message to registration inputs and password hashing

--- album-roulette/src/Register.jsx
+++ album-roulette/src/Register.jsx
@@ -1,21 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import axios from 'axios'
+import {createHash } from 'crypto'
+
 
 export default function RegisterNew(){
-    const [usernameReg, setusernameReg] = useState("")
-    const [PasswordReg, setPasswordReg] = useState("")
-    const [matchPassword, setMatchPassword] = useState("")
-    let [IsMatching, setIsMatching]= useState(true)
-    let [IsAvailable, setIsAvailable] = useState(true);
+    const [IsAvailable, setIsAvailable] = useState(true);
 
-    const hash = () =>{
+    const [userState, setUserState] = useState({
+        username:"",
+        password: "",
+        confirmPassword: "",
 
+    })
+
+
+    const handleChange = (e) =>{
+        e.preventDefault();
+        const {name, value} = e.target;
+        setUserState((values) => ({
+            ...values,
+            [name] : value
+        }))
+        setIsAvailable(true)
     }
+
+    
+  
+    const hashpassword = (string) =>{
+        return createHash('sha256').update(string).digest('hex');
+    }
+
+
     const register = () => {
         axios.post('http://localhost:5174/api/post/register', {
         
-            username: usernameReg, 
-            password: PasswordReg,
+            username: userState.username, 
+            password: hashpassword(userState.password),
             
         }).then((response) => {
             console.log(response)
@@ -26,7 +46,7 @@ export default function RegisterNew(){
             const response = await axios.get(`http://localhost:5174/api/get/username/${name}`);
             for (let i = 0; i < response.data.recordset.length; i++) {
                 if (response.data.recordset[i].Username.toLowerCase() === name.toLowerCase()) {
-        
+                    setIsAvailable(false)
                     return false;
                     
                 } else {
@@ -38,7 +58,7 @@ export default function RegisterNew(){
             return true;
     }
     const CheckPassword = () =>{
-        if(PasswordReg.toLowerCase() == matchPassword.toLowerCase()){
+        if(userState.password.toLowerCase() == userState.confirmPassword.toLowerCase()){
             
             return true
         }
@@ -51,11 +71,10 @@ export default function RegisterNew(){
     const wrapper = async (e) => {
         e.preventDefault();
         
-            var noe = await CheckUsername(usernameReg);
+            var noe = await CheckUsername(userState.username);
             var noe2 = await CheckPassword();
                 if(noe && noe2){
                     console.log("Kjør registrering")
-                    
                     register();
                 }
                 else {
@@ -68,13 +87,20 @@ export default function RegisterNew(){
         <div className="flex items-center justify-center">
             <div className="flex flex-col items-center justify-center h-72 w-72 mt-12 border-solid border-2">
                 <form className="flex flex-col" >
-                    <label >Username:</label> <input type="username" onChange={(e) => {setusernameReg(e.target.value)}}/>
-                    {IsAvailable? <></> : <p className="text-red-600">{usernameReg} already exists</p>}
-                    <label>Password:</label><input type="password" onChange={(e) => {setPasswordReg(e.target.value)}} />
-                    <label>Repeat password: </label><input type="password" onChange={(e) => {setMatchPassword(e.target.value)}}/>
-                    {IsMatching ? <></> : <p className="text-red-600">Password do not match!</p>}
-                    <button onClick={wrapper}>Submit</button>
-                    <button onClick={wrapper}>Testknapp</button>
+                    <label >Username:</label> 
+                    <input type="username" name="username" className="border-solid border-2" onChange={handleChange}/>
+
+                    {IsAvailable? <></> : <p className="text-red-600">{userState.username} already exists</p>}
+
+                    <label>Password:</label>
+                    <input name="password" className="border-solid border-2" type="password" onChange={handleChange} />
+
+                    <label>Repeat password: </label>
+                    <input name="confirmPassword" className="border-solid border-2" type="password" onChange={handleChange}/>
+
+                    {userState.password.toLowerCase() == userState.confirmPassword.toLowerCase()? <></> : <p className="text-red-600">Password do not match!</p>}
+
+                    <button onClick={wrapper}>Register</button>
                 </form>
             </div>
         </div>
